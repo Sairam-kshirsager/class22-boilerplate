7dfcd494f66bf7da90928f9f11a3931624660720
Sairam-kshirsager <70512559+Sairam-kshirsager@users.noreply.github.com>
2020-09-10 15:26:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+const Engine = Matter.Engine;
+ const World = Matter.World;
+  const Bodies = Matter.Bodies;
+   var world,engine;
+   
+   function setup(){
+         var canvas = createCanvas(400,400);
+ engine = Engine.create();
+  world = engine.world;
+   var object_options = {
+        isstatic: false 
+    }
+    object = Bodies.rectangle(200,100,50,50,object_options);
+    World.add(world,object);
+    
+    console.log(object); 
+      console.log(object.position.x);
+       console.log(object.position.y); 
+    }
+     function draw(){
+          background(0);
+           Engine.update(engine); 
+           rectMode(CENTER);
+            rect(object.position.x,object.position.y,50,50);
+        }
